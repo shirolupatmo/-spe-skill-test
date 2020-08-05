c5745f5bb4ecc103542efdd17574d88e8b6ee009
Hery <hery.kurniawan95@gmail.com>
2020-08-05 15:09:17 +0700

    2

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 
+	$arrnumber = [160, 3, 1719, 19, 11, 13, -21];
+
 	narcissistic(111);
+	parityoutlier($arrnumber);
+
 
 	function narcissistic($numb){
 		$sumcar = strval($numb);
@@ -21,6 +25,40 @@
 			echo 'true';
 		}else{
 			echo 'false';
+		}
+	}
+
+	function parityoutlier($arr){
+		echo '<br> ----- <br>';
+
+		$oddnumber 	= 0;
+		$ganjil = '';
+		$evennumber = 0;
+		$genap = '';
+
+		foreach($arr as $character){
+			if((int)$character >= 0){
+				if((int)$character%2 == 0){
+					$evennumber += 1;
+					$genap = $character;
+				}else{
+					$oddnumber += 1;
+					$ganjil = $character;
+				}
+			}
+		}
+
+		echo 13%2;
+
+		//echo "ganjil = ".$oddnumber." - genap = ".$evennumber;
+
+		if($oddnumber == 1){
+			echo $ganjil." (the only odd number)";
+		}elseif($evennumber == 1){
+			echo $ganjil." (the only even number)";
+		}else{
+			//echo "all odd integer, no outlier";
+			echo "ganjil = ".$oddnumber." - genap = ".$evennumber;
 		}
 	}
 
